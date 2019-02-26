920824facdeed75875f64e059bc563cb1b29fe60
Ali Vakilzade <ali@vakilzade.com>
2019-02-26 13:43:55 +0330

    code moved to dir

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/aliva/pinggraph
 
 require (
 	github.com/gorilla/websocket v1.4.0
-	golang.org/x/crypto v0.0.0-20190219172222-a4c6cb3142f2
-	golang.org/x/sys v0.0.0-20190225065934-cc5685c2db12 // indirect
+	golang.org/x/crypto v0.0.0-20190225124518-7f87c0fbb88b
 	gopkg.in/yaml.v2 v2.2.2
 )
--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/aliva/pinggraph/pinggraph"
 )
 
 func main() {
 	fileFlag := flag.String("f", "nodes.yml", "hosts file")
 	flag.Parse()
 
-	nodes := loadNodes(*fileFlag)
+	nodes := pinggraph.LoadNodes(*fileFlag)
 
 	for _, n := range nodes {
 		for _, r := range nodes {
@@ -21,12 +23,12 @@ func main() {
 		}
 	}
 
-	go webSocketHandler()
+	go pinggraph.WebSocketHandler()
 
 	fmt.Println("Open http://127.0.0.1:8000 in browser")
 
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", serveWs)
+	http.HandleFunc("/", pinggraph.ServeHome)
+	http.HandleFunc("/ws", pinggraph.ServeWs)
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
